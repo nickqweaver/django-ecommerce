adb0280867bdb019e3b29c62d22a02611ef9f100
nickqw14 <nickqw14@gmail.com>
2021-08-25 22:59:52 -0600

    feat: all queries are accessible now

--- api/schema.py
+++ api/schema.py
@@ -4,7 +4,7 @@ from product.graphql.query import ProductQuery
 from category.graphql.query import CategoryQuery
 
 
-class Query(ProductQuery):
+class Query(ProductQuery, CategoryQuery):
     pass
 
 
