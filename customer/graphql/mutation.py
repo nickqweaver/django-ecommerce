3c0754ea024d2b9ab6a466111fcf59abc75eab1b
nickqw14 <nickqw14@gmail.com>
2021-11-15 22:49:47 -0700

    feat: address mutations written and are working correctly

--- customer/graphql/mutation.py
+++ customer/graphql/mutation.py
@@ -1,7 +1,10 @@
+from customer.graphql.types import AddressInput, AddressType
+from customer.models import STATE_CHOICES
 from graphql import GraphQLError
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from order.models import OrderItem, Order
-from graphene import ObjectType, String, ID, Boolean, Mutation
+from graphene import ObjectType, String, ID, Boolean, Mutation, Field, Int, Enum
+from customer.models import Address
 
 
 class CreateCustomer(Mutation):
@@ -25,6 +28,49 @@ class CreateCustomer(Mutation):
     
     return CreateCustomer(user_id=user.id, success=True)
 
+class UpdateAddress(Mutation):
+  success = Boolean()
+
+  class Arguments:
+    user_id = ID()
+    address = AddressInput()
+
+  def mutate(root, self, address, id):
+    user_profile = get_user_model().objects.get(pk=id).profile
+    ## Get or Create address object here and add profile to it ?
+    ## How do we handle the fact that a user can have multiple address's?
+    ## Maybe we have add address mutation and update address mutation, to update address we
+    ## would just need to pass the address ID up instead of needing to worry about the user's info
+    ## Then if user does not have address we are just simply creating one and attaching their profile
+    ## to it.      
+    return UpdateAddress(success=True)
+
+class CreateAddress(Mutation):
+  success = Boolean()
+  address = Field(AddressType)
+
+  class Arguments:
+    address = AddressInput()
+    user_id = ID()
+
+  def mutate(root, info, address, user_id):
+    new_address = None
+    success = True
+    try:
+      ## TODO - get_or_create will always create new even if everything is the same except 1 field. 
+      ## Should handle this better by checking if the address object has comparable values and update instead of create
+      profile = get_user_model().objects.get(pk=user_id).profile
+      new_address, created = Address.objects.get_or_create(profile=profile, address1=address.line_1, address2=address.line_2, zip_code=address.zip, state=address.state, city=address.city, country=address.country)
+    except:
+      success = False
+      raise Exception("Could not add address to profile")
+
+
+    return CreateAddress(success=success, address=new_address)
+
+
 
 class CustomerMutations(ObjectType):
   create_customer = CreateCustomer.Field()
+  create_address = CreateAddress.Field()
+  update_address = UpdateAddress.Field()
--- customer/graphql/types.py
+++ customer/graphql/types.py
@@ -1,4 +1,19 @@
 
 import graphene
-from graphene import String
+from graphene import String, Int, Enum, Field
+from graphene_django import DjangoObjectType
+from customer.models import COUNTRIES, STATE_CHOICES, Address
 
+class AddressInput(graphene.InputObjectType):
+    line_1 = String(required=True)
+    line_2 = String()
+    city = String(required=True)
+    zip = Int(required=True)
+    # state = Field(Enum('State', STATE_CHOICES)) TODO - Change back to Enums later, passes value and we need the enum member to store in db
+    # country = Field(Enum('Country_Code', COUNTRIES))
+    state = String(required=True)
+    country = String(required=True)
+
+class AddressType(DjangoObjectType):
+  class Meta:
+    model = Address
